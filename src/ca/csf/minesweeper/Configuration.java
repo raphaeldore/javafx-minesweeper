970871d63c1f93dec9e1e1e367206111b48451c8
Raphaël Doré <rdore@neomalibox.ch>
2014-10-18 13:36:55 -0400

    Implemented GameDifficulty Enum

--- src/ca/csf/minesweeper/Configuration.java
+++ src/ca/csf/minesweeper/Configuration.java
@@ -1,5 +1,30 @@
 package ca.csf.minesweeper;
 
+/*
+ * All constants are defined here. As are all user defined options (Such as the selected
+ * difficulty).
+ */
 public class Configuration {
   // TODO: Enum TileState {Normal, Flag, QuestionMark}
+  // public static final nbrOfTiles
+
+  public static GameDifficulty selectedGameDifficulty;
+
+  public static enum GameDifficulty {
+    Beginner("Débutant", 9, 9, 10), Intermediate("Intermédiaire", 16, 16, 40), Expert("Expert", 30,
+        16, 99);
+
+    public final String difficultyName;
+    public final int gameBoardWidth;
+    public final int gameBoardHeight;
+    public final int nbrOfMines;
+
+    private GameDifficulty(String difficultyName, int gameBoardWidth, int gameBoardHeight,
+        int nbrOfMines) {
+      this.difficultyName = difficultyName;
+      this.gameBoardWidth = gameBoardWidth;
+      this.gameBoardHeight = gameBoardHeight;
+      this.nbrOfMines = nbrOfMines;
+    }
+  }
 }
