540d4afd981c1b9cd563c73442fd0000dc9b6546
Raphaël Doré <rdore@neomalibox.ch>
2014-10-18 21:52:36 -0400

    Add MinesweeperGame.java. A class that will implement the basic gameloop

---
+++ src/ca/csf/minesweeper/model/MinesweeperGame.java
@@ -0,0 +1,14 @@
+package ca.csf.minesweeper.model;
+
+/*
+ * 
+ * Main game loop class
+ * 
+ */
+
+public class MinesweeperGame {
+  public static void main(String[] args) {
+    GameBoard gameBoard = new GameBoard<GameTile>();
+
+  }
+}
