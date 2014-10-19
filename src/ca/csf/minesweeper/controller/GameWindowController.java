4d98c409fa5a8d171db95843262850a7b842dc74
Raphaël Doré <rdore@neomalibox.ch>
2014-10-18 22:50:40 -0400

    Implemented basic timer framework, I.E The timer() and updateTimer() methods. With the help of the JavaFX timeline http://docs.oracle.com/javafx/2/api/javafx/animation/Timeline.html)

--- src/ca/csf/minesweeper/controller/GameWindowController.java
+++ src/ca/csf/minesweeper/controller/GameWindowController.java
@@ -2,25 +2,33 @@ package ca.csf.minesweeper.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Timer;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.beans.property.BooleanProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
-import ca.csf.minesweeper.model.Subject;
-import ca.csf.minesweeper.model.Observer;
+import javafx.util.Duration;
+import ca.csf.minesweeper.model.GameState;
 import ca.csf.simpleFx.SimpleFXController;
 import ca.csf.simpleFx.SimpleFXStage;
 import ca.csf.simpleFx.dialogs.SimpleFXDialogChoiceSet;
 import ca.csf.simpleFx.dialogs.SimpleFXDialogIcon;
 import ca.csf.simpleFx.dialogs.SimpleFXDialogResult;
 import ca.csf.simpleFx.dialogs.SimpleFXDialogs;
+import javafx.scene.control.Label;
 
 public class GameWindowController extends SimpleFXController implements Initializable {
 
   private SimpleFXStage parentStage;
-
+  private Timer timer;
+  private GameState gameState;
+  private Integer timePlayed = new Integer(980);
+  private Timeline timeline;
   private ToggleButton toggleButton = new ToggleButton();
 
   public void initialize(SimpleFXStage stage) {
@@ -29,6 +37,8 @@ public class GameWindowController extends SimpleFXController implements Initiali
 
   @FXML
   public Button btnPatate;
+  @FXML
+  Label lblLabel1;
 
   @FXML
   public void patate() {
@@ -47,6 +57,29 @@ public class GameWindowController extends SimpleFXController implements Initiali
     }
   }
 
+  public void timer() {
+    // Basic timer template
+    timeline =
+        new Timeline(new KeyFrame(Duration.millis(1000),
+            actionEvent -> lblLabel1.setText(updateTimer()))); // TODO: Placeholder. Eventually
+                                                               // replace with
+                                                               // gameState.incrementTimePlayedByOneSecond()
+    timeline.setCycleCount(Animation.INDEFINITE);
+    timeline.play();
+  }
+
+  public String updateTimer() {
+    if (timePlayed == 998) {
+      timeline.stop();
+    }
+
+    timePlayed++;
+    return timePlayed.toString();
+  }
+
   @Override
-  public void initialize(URL location, ResourceBundle resources) {}
+  public void initialize(URL location, ResourceBundle resources) {
+    // gameState = new GameState();
+    timer();
+  }
 }
