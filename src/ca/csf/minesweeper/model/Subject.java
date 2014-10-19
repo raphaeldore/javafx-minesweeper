57a399ffd8e51b70a803bd3d176d88eb39296399
Raphaël Doré <rdore@neomalibox.ch>
2014-10-18 20:10:19 -0400

    Implemented a basic Subject class.
    It's still a work in progress, some methods still need to be implemented (Such as clearObservers).

---
+++ src/ca/csf/minesweeper/model/Subject.java
@@ -0,0 +1,42 @@
+package ca.csf.minesweeper.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class Subject<T> {
+  private List<Observer<T>> observers;
+
+  public Subject() {
+    this.observers = new ArrayList<Observer<T>>();
+  }
+
+  public void addObserver(Observer<T> observer) {
+    if (observer == null) {
+      throw new NullPointerException();
+    }
+    
+    // We do not want to be adding the same observer twice
+    if (!observers.contains(observer)) {
+      observers.add(observer);
+    }
+  }
+
+  public void removeObserver(Observer<T> observer) {
+    if (!observers.contains(observer)) {
+      throw new NoSuchElementException();
+    }
+    
+    observers.remove(observer);
+  }
+
+  protected void notifyObservers(T argument) {
+    for (Observer<T> observer : observers) {
+      observer.update(this, argument);
+    }
+  }
+
+  public int countObservers() {
+    return observers.size();
+  }
+}
