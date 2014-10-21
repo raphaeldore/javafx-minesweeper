a41a8e8309972ef9ca7873172c22d0e9992f72d6
Raphaël Doré <rdore@neomalibox.ch>
2014-10-21 14:46:53 -0400

    Implemented methods to ease the use of a timer in the GameWindowController

--- src/ca/csf/minesweeper/model/GameState.java
+++ src/ca/csf/minesweeper/model/GameState.java
@@ -1,8 +1,34 @@
 package ca.csf.minesweeper.model;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class GameState {
   /*
    * TODO: -Timer -Flags -nbrOfRevealedTiles
    */
+
+  private int flagCount;
+  private AtomicInteger timePlayed; // TODO: When the game starts, the JavaFX timer starts (In the GameWindow Controller).
+
+  public static enum GameStates {
+    START, PAUSE, PLAYING, WON;
+  }
+
+  /**
+   * @return the timePlayed
+   */
+  public AtomicInteger getTimePlayed() {
+    return timePlayed;
+  }
+
+  /**
+   * @param timePlayed the timePlayed to set
+   */
+  public void incrementTimePlayedByOneSecond() {
+    //timePlayed++;
+    timePlayed.addAndGet(1);
+  }
+  
+  
 
 }
