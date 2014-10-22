debf6b5d014a6aab01fd089b39748ebb25d8e5e4
Raphaël Doré <rdore@neomalibox.ch>
2014-10-22 09:45:11 -0400

    GameTile was not extending the right class, so I changed that. In consequence, unrelated methods were removed.

--- src/ca/csf/minesweeper/model/GameTile.java
+++ src/ca/csf/minesweeper/model/GameTile.java
@@ -1,24 +1,14 @@
 package ca.csf.minesweeper.model;
 
 
-public class GameTile<T> extends Observer<T> {
+public class GameTile extends Subject<GameTile> {
 
-  public GameTile() {}
-
-  public GameTile(Subject<T> subject) {
-    this.subject = subject;
-    this.subject.addObserver(this);
-  }
-
-  @Override
-  public void update() {
-    // TODO: Implement this empty method
-  }
-
-  @Override
-  public void update(Subject<T> sender, T argument) {
-    System.out.print(argument.toString());
+  public GameTile() {
 
   }
-
+  
+  public void revealGameTile()
+  {
+    
+  }
 }
