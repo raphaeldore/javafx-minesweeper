7248b7878ceb821791188d555486b6b6b1008899
Samuel <samuel.theriault.hall@gmail.com>
2014-10-24 11:04:03 -0400

    added GameBoard tiles generation as well as mine generation, handling the neighboring mines count (also added to GameTile)

--- src/ca/csf/minesweeper/model/GameBoard.java
+++ src/ca/csf/minesweeper/model/GameBoard.java
@@ -1,19 +1,49 @@
 package ca.csf.minesweeper.model;
 
-import java.util.ArrayList;
+import java.util.Random;
 
 /*
  * 
  * Represents a grid of GameTile instances.
- * 
  */
 
 public class GameBoard extends Subject<GameBoard> {
-  
-  ArrayList<GameTile> tiles;
-  
-  public GameBoard() {
-    tiles = new ArrayList<GameTile>();
+
+  private GameTile[][] tiles;
+
+  public GameBoard(int rowSize, int columnSize, int mineCount) {
+    tiles = new GameTile[rowSize][columnSize];
+    for (int i = 0; i < columnSize; i++) {
+      for (int j = 0; j < rowSize; j++) {
+        tiles[j][i] = new GameTile();
+      }
+    }
+
+
+    while (mineCount > 0) {
+      Random random = new Random();
+      int x = random.nextInt(rowSize - 1);
+      int y = random.nextInt(columnSize - 1);
+
+      if (tiles[x][y].setAsMine()) {
+        incrementExistingTile(x - 1, y - 1);
+        incrementExistingTile(x - 1, y);
+        incrementExistingTile(x, y - 1);
+        incrementExistingTile(x + 1, y - 1);
+        incrementExistingTile(x + 1, y + 1);
+        incrementExistingTile(x - 1, y + 1);
+        incrementExistingTile(x + 1, y);
+        incrementExistingTile(x, y + 1);
+        mineCount--;
+      }
+    }
+
+  }
+
+  public void incrementExistingTile(int row, int column) {
+    if (row >= 0 && column >= 0) {
+      tiles[row][column].incrementNeighboringMineCount();
+    }
   }
 
 }
--- src/ca/csf/minesweeper/model/GameTile.java
+++ src/ca/csf/minesweeper/model/GameTile.java
@@ -3,12 +3,27 @@ package ca.csf.minesweeper.model;
 
 public class GameTile extends Subject<GameTile> {
 
+  private boolean isMine = false;
+  private int neighboringMineCount = 0;
+
   public GameTile() {
 
   }
-  
-  public void revealGameTile()
-  {
-    
+
+  public void revealGameTile() {
+
+  }
+
+  public boolean setAsMine() {
+    if (isMine) {
+      return false;
+    } else {
+      isMine = true;
+      return true;
+    }
+  }
+
+  public void incrementNeighboringMineCount() {
+    neighboringMineCount++;
   }
 }
