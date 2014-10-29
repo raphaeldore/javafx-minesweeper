f325af01a3842e536d905d31ed21ce763283f20d
Raphaël Doré <rdore@neomalibox.ch>
2014-10-28 22:25:44 -0400

    The Stage is now in a PAUSE state when the Window loses focus. For now it only prints the words "FOCUS" and "NO FOCUS" in the console, but in the forseable future it should call the Pause method in the GameWindowController

--- src/ca/csf/minesweeper/Main.java
+++ src/ca/csf/minesweeper/Main.java
@@ -1,11 +1,14 @@
 package ca.csf.minesweeper;
 
+import javafx.event.EventHandler;
 import javafx.stage.StageStyle;
 import ca.csf.minesweeper.controller.GameWindowController;
+import ca.csf.minesweeper.model.GameState.GameStates;
 import ca.csf.simpleFx.SimpleFXApplication;
 import ca.csf.simpleFx.SimpleFXApplicationLauncher;
 import ca.csf.simpleFx.SimpleFXScene;
 import ca.csf.simpleFx.SimpleFXStage;
+import ca.csf.simpleFx.events.WindowFocusEvent;
 
 public class Main extends SimpleFXApplication {
   public static void main(String[] args) {
@@ -22,6 +25,21 @@ public class Main extends SimpleFXApplication {
       SimpleFXStage simpleFXStage =
           new SimpleFXStage("Démineur", StageStyle.DECORATED, simpleFXScene, this);
       simpleFXStage.centerOnScreen();
+      simpleFXStage.setOnFocusChanged(new EventHandler<WindowFocusEvent>() {
+
+        @Override
+        public void handle(WindowFocusEvent event) {
+          if (simpleFXStage.isFocused()) {
+            System.out.println("FOCUS");
+            if (Configuration.currentGameState == GameStates.PAUSE) {
+              Configuration.currentGameState = GameStates.PLAYING;
+            }
+          } else {
+            System.out.println("NO FOCUS");
+            Configuration.currentGameState = GameStates.PAUSE;
+          }
+        }
+      });
       simpleFXStage.show();
     } catch (Exception ex) {
       ex.printStackTrace();
