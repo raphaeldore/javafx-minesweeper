e32f5932a5051da88a52cf81f61e6804aed541e9
Raphaël Doré <rdore@neomalibox.ch>
2014-10-30 22:12:31 -0400

    The HelpWindowController implements the initializable interface, making it possible to load the html help file when the stage is loaded. I also load some custom fonts so that I can use them in Aide.html.

--- src/ca/csf/minesweeper/controller/HelpWindowController.java
+++ src/ca/csf/minesweeper/controller/HelpWindowController.java
@@ -1,7 +1,44 @@
 package ca.csf.minesweeper.controller;
 
+import static ca.csf.minesweeper.controller.ControllerConsts.resourcesPath;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.text.Font;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 import ca.csf.simpleFx.SimpleFXController;
 
-public class HelpWindowController extends SimpleFXController {
+public class HelpWindowController extends SimpleFXController implements Initializable {
+
+  @FXML
+  WebView webView;
+  @FXML
+  Button btnOk;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    Font.loadFont(HelpWindowController.class.getResource(resourcesPath + "Enriqueta-Regular.ttf")
+        .toExternalForm(), 10);
+
+    Font.loadFont(HelpWindowController.class.getResource(resourcesPath + "Enriqueta-Bold.ttf")
+        .toExternalForm(), 10);
+    
+    btnOk.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent e) {
+          Stage stage = ((Stage) btnOk.getScene().getWindow());
+          stage.close();
+      }
+  });
+
+    webView.getEngine().load(getClass().getResource(resourcesPath + "Aide.html").toExternalForm());
+  }
 
 }
