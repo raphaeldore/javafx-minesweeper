7c8d222c546eff7c25aa79dd9348fb8efd2a2cec
Raphaël Doré <rdore@neomalibox.ch>
2014-11-02 20:14:41 -0500

    HighScoresWindowController now implements the initializable interface, which means the class now has an initialize method, which I used to add an event to the ok button (when is is press, the window closes..How fun!)

--- src/ca/csf/minesweeper/controller/HighScoresWindowController.java
+++ src/ca/csf/minesweeper/controller/HighScoresWindowController.java
@@ -1,7 +1,31 @@
 package ca.csf.minesweeper.controller;
 
-import ca.csf.simpleFx.SimpleFXController;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HighScoresWindowController extends SimpleFXController {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
+import ca.csf.simpleFx.SimpleFXController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class HighScoresWindowController extends SimpleFXController implements Initializable {
+
+  @FXML Button btnOk;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    btnOk.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent e) {
+        Stage stage = ((Stage) btnOk.getScene().getWindow());
+        stage.close();
+      }
+    });
+  }
+  
+  
 
 }
