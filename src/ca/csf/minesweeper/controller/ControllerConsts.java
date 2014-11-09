475be6af701f2e2ada0970e8a1ee36eca642e188
Raphaël Doré <rdore@neomailbox.ch>
2014-11-09 12:40:01 -0500

    Added todos to remind me that some image aren,t used in the game

--- src/ca/csf/minesweeper/controller/ControllerConsts.java
+++ src/ca/csf/minesweeper/controller/ControllerConsts.java
@@ -20,13 +20,13 @@ final class ControllerConsts {
 	public static final Image IMAGE_EIGHT_MINES = loadImage("8.png");
 	public static final Image IMAGE_FLAG = loadImage("Flag.png");
 	public static final Image IMAGE_MINE = loadImage("Mine.png");
-	public static final Image IMAGE_MINE_CROSS = loadImage("MineCross.png");
+	public static final Image IMAGE_MINE_CROSS = loadImage("MineCross.png"); // TODO: This is never used
 	public static final Image IMAGE_MINE_RED = loadImage("MineRed.png");
 	public static final Image IMAGE_QUESTION_MARK = loadImage("QuestionMark.png");
 	public static final Image IMAGE_SMILE_DEAD = loadImage("Smile_Dead.png");
 	public static final Image IMAGE_SMILE_HAPPY = loadImage("Smile_Happy.png");
 	public static final Image IMAGE_SMILE_NORMAL = loadImage("Smile_Normal.png");
-	public static final Image IMAGE_SMILE_WORRY = loadImage("Smile_Worry.png");
+	public static final Image IMAGE_SMILE_WORRY = loadImage("Smile_Worry.png"); // TODO: This is never used
 
 	private static Image loadImage(String path) {
 		InputStream resource = ControllerConsts.class
