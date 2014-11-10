263e69bcd45580aff42f2f33a6940e3042b58b1c
Raphaël Doré <rdore@neomailbox.ch>
2014-11-09 19:27:11 -0500

    Removed comment saying that this was a class with all the constants (Which isn,t true..there isn't even one)

--- src/ca/csf/minesweeper/Configuration.java
+++ src/ca/csf/minesweeper/Configuration.java
@@ -3,10 +3,6 @@ package ca.csf.minesweeper;
 import ca.csf.minesweeper.model.GameDifficulty;
 import ca.csf.minesweeper.model.GameStates;
 
-/*
- * All constants are defined here. As are all user defined options (Such as the selected
- * difficulty).
- */
 public class Configuration {
   public static GameDifficulty selectedGameDifficulty = GameDifficulty.BEGINNER;
   public static GameStates currentGameState = GameStates.PLAYING;   // Initial GameState
